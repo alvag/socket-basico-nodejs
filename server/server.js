2392f186ba84ddb9e08b47c6244ad495b6435e08
Max Alva <alva85@gmail.com>
2018-11-19 17:34:50 -0500

    Configurando socket.io frontend

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const path = require('path');
 require('dotenv').config();
-
+const socketIO = require('socket.io');
+const http = require('http');
 const express = require('express');
 const app = express();
+
+let server = http.createServer(app);
 
 const bodyParser = require('body-parser');
 
@@ -13,7 +16,16 @@ app.use(express.static(path.resolve(__dirname, '../public')));
 
 app.use(require('./routes/'));
 
-app.listen(process.env.PORT, () => {
+let io = socketIO(server);
+
+io.on('connection', (client) => {
+    console.log('Usuario conectado');
+});
+
+server.listen(process.env.PORT, (error) => {
+
+    if (error) throw new Error(error);
+
     // eslint-disable-next-line no-console
     console.log(`Servidor corriendo en el puerto ${process.env.PORT}.`);
 });
